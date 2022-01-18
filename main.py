25ed4a1f76b965785764e949078566cb1e4057fd
swipswip <cripter2004@gmail.com>
2022-01-18 11:28:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,28 +6,33 @@ clock = pygame.time.Clock()
 
 pikasize = 305;
 pika_IMG = pygame.image.load("pikapika.png")
+nyaon_IMG = pygame.image.load("nya.png")
 pika = pika_IMG.get_rect()
+nya = nyaon_IMG.get_rect()
 pika.centerx = 500
 pika.centery = 500
-
+nya.centerx = 800
+nya.centery = 800
 while True:
     screen.fill((0, 0, 0))
     
     event = pygame.event.poll()
+    pygame.key.set_repeat(1, 1)
     if event.type == pygame.QUIT:
         break
-    elif event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-            pika.left -= 5
-        elif event.key == pygame.K_RIGHT:
-            pika.left += 5
-        elif event.key == pygame.K_UP:
-            pika.top -= 5
-        elif event.key == pygame.K_DOWN:
-            pika.top += 5
+    key_event = pygame.key.get_pressed()
+    if key_event[pygame.K_LEFT]:
+        pika.left -= 5
+    elif key_event[pygame.K_RIGHT]:
+        pika.left += 5
+    elif key_event[pygame.K_UP]:
+        pika.top -= 5
+    elif key_event[pygame.K_DOWN]:
+        pika.top += 5
     
     #수정 완료
     screen.blit(pika_IMG, pika)
+    screen.blit(nyaon_IMG, nya)
     pygame.display.update()
     clock.tick(30)
     
