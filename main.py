b041e6fef25e5655e009bb3790646838b264091a
swipswip <cripter2004@gmail.com>
2022-02-04 17:59:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.init()
 screen = pygame.display.set_mode()
@@ -16,21 +17,50 @@ pikawidth = 300
 pikaheight = 256
 
 
-pika_IMG = ["", "", "", "", "", "", "", "", "", "", ""]
-pika_IMG[0] = pygame.image.load("Asset\Sprites\Main_Character\player\idle\player_idle_0.png")
+pika_IMG = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+pika_IMG[0] = pygame.image.load("./Asset/Sprites/Main_Character/Player/idle/player_idle_0.png")
+bg_IMG = ["", "", "", "", "", ""]
+cave = ["", "", "", "", "", ""]
 
-for i in range(1, 10):
-    pika_IMG[i] = pygame.image.load("Asset\Sprites\Main_Character\player\run\player_run_0" + str(i - 1) + ".png")
+for i in range(1, 11):
+    aaa = "./Asset/Sprites/Main_Character/Player/moving/player_run_0" + str(i - 1) + ".png"
+    pika_IMG[i] = pygame.image.load(aaa)
     
-nyaon_IMG = pygame.image.load("nya.png")
+for i in range(11, 22):
+    pika_IMG[i] = pygame.transform.flip(pika_IMG[i - 11], True, False)
+    
+for i in range(0, 5):
+    aaa = "./Asset/Sprites/Cave_Background/bg_cave_" + str(i + 1) + ".png"
+    bg_IMG[i] = pygame.image.load(aaa)
+    cave[i] = bg_IMG[i].get_rect()
+    cave[i].left = 0
+    
+#nyaon_IMG = pygame.image.load("nya.png")
 
 pika = pika_IMG[0].get_rect()
-nya = nyaon_IMG.get_rect()
+#nya = nyaon_IMG.get_rect()
 pika.top = sy - pika.height
-pika.left = 0
-nya.centerx = 900
-nya.centery = 900
-pis = 0;
+pika.left = sx / 2 - pika.width / 2
+#nya.centerx = 900
+#nya.centery = 900
+pis = 0
+leftfocus = 1
+dd = 0
+pikap = 0
+
+def moving(d):
+    
+    
+    global leftfocus, pis, pikap
+    #pika.left = pika.left + d * 10
+    leftfocus = d
+    pis = (pis + 1) % 11
+    pikap += d
+    for i in range(0, 5):
+        ii = 4 - i
+        cave[ii].left += -d * i * 2
+
+
 
 while True:
     screen.fill((0, 0, 0))
@@ -41,19 +71,17 @@ while True:
         break
     key_event = pygame.key.get_pressed()
     if key_event[pygame.K_LEFT]:
-        pika.left -= 10
-        pika = (pika + 1) % 11
-    if key_event[pygame.K_RIGHT]:
-        pika.left += 10
-        pika = (pika + 1) % 11
+        moving(-1)
+        dd = 11
+    elif key_event[pygame.K_RIGHT]:
+        moving(1)
+        dd = 0
+        
     if key_event[pygame.K_UP] and g < 1:
         g = 1 #중력 시스템 ON
         jumps = jump
-    if event.type == pygame.KEYUP and g < 1:
-        pika = 0
-        
-    if (pika.top < nya.bottom and nya.top < pika.bottom and pika.left < nya.right and nya.left < pika.right):
-        break
+    #if (pika.top < nya.bottom and nya.top < pika.bottom and pika.left < nya.right and nya.left < pika.right):
+        #break
     
     if g > 0:
         pika.top = pika.top - jumps
@@ -65,9 +93,18 @@ while True:
     
     #수정 완료
     
-    screen.blit(pika_IMG[pika], pika)
-    screen.blit(nyaon_IMG, nya)
+    for i in range(0, 5):
+        ii = 4 - i
+        screen.blit(bg_IMG[ii], cave[ii])
+        if (sy.width > cave[ii].left + cave[ii].width):
+            screen.blit(bg_IMG[ii], pygame.Rect(cave[ii].left + cave[ii].width, cave[ii].top, cave[ii].left + cave[ii].width + cave[ii].width, cave[ii].height))
+        elif (sy.left < cave[ii].left):
+            screen.blit(bg_IMG[ii], pygame.Rect(cave[ii].left - cave[ii].width, cave[ii].top, cave[ii].left, cave[ii].height))
+        
+    screen.blit(pika_IMG[pis + dd], pika)
+    
+    #screen.blit(nyaon_IMG, nya)
     pygame.display.update()
-    clock.tick(30)
+    clock.tick(20)
     
 pygame.quit()
