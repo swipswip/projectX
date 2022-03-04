824f49cef2e3bd794c7c2fa87d4e4dd318ee73a8
swipswip <97379765+swipswip@users.noreply.github.com>
2022-03-04 13:44:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,7 @@ pikaheight = 256
 
 pika_IMG = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
 pika_IMG[0] = pygame.image.load("./Asset/Sprites/Main_Character/Player/idle/player_idle_0.png")
-bg_IMG = ["", "", "", "", "", ""]
-cave = ["", "", "", "", "", ""]
+
 
 for i in range(1, 11):
     aaa = "./Asset/Sprites/Main_Character/Player/moving/player_run_0" + str(i - 1) + ".png"
@@ -31,11 +30,11 @@ for i in range(1, 11):
 for i in range(11, 22):
     pika_IMG[i] = pygame.transform.flip(pika_IMG[i - 11], True, False)
     
-for i in range(0, 5):
-    aaa = "./Asset/Sprites/Cave_Background/bg_cave_" + str(i + 1) + ".png"
-    bg_IMG[i] = pygame.image.load(aaa)
-    cave[i] = bg_IMG[i].get_rect()
-    cave[i].left = 0
+bg_IMG = pygame.image.load("./asset\Sprites/needs/cave_bg.png")
+bg_IMG = pygame.transform.scale(bg_IMG, (sx, sy))
+cave = bg_IMG.get_rect()
+cave.left = 0
+
     
 block_IMG = pygame.image.load("./Asset/Sprites/Tileset/stonefloor.png")
 block = block_IMG.get_rect()
@@ -62,25 +61,12 @@ def moving(d):
     elif bn >= block.width:
         bn -= block.width
         
-    for i in range(0, 5):
-        ii = 4 - i
-        cave[ii].left += -d * i * 3
         
         
 
 def printscreen():
-    for i in range(0, 5):
-        ii = 4 - i
-        if (sx > cave[ii].left + cave[ii].width):
-            screen.blit(bg_IMG[ii], pygame.Rect(cave[ii].left + cave[ii].width, cave[ii].top, cave[ii].left + cave[ii].width + cave[ii].width, cave[ii].height))
-            if (cave[ii].left + cave[ii].width <= 0):
-                cave[ii].left = 0
-        elif (0 <= cave[ii].left):
-            screen.blit(bg_IMG[ii], pygame.Rect(cave[ii].left - cave[ii].width, cave[ii].top, cave[ii].left, cave[ii].height))
-            if (cave[ii].left >= sx):
-                cave[ii].left = sx - cave[ii].width
                    
-        screen.blit(bg_IMG[ii], cave[ii])
+    screen.blit(bg_IMG, cave)
         
     screen.blit(pika_IMG[pis + dd], pika)
     
@@ -104,13 +90,9 @@ while True:
     if event.type == pygame.QUIT:
         break
     key_event = pygame.key.get_pressed()
-    if key_event[pygame.K_LEFT]:
-        moving(-1)
-        dd = 11
-    elif key_event[pygame.K_RIGHT]:
-        moving(1)
-        dd = 0
-    elif key_event[pygame.K_ESCAPE]:
+
+    moving(1)
+    if key_event[pygame.K_ESCAPE]:
         sys.exit()
     if key_event[pygame.K_UP] and g < 1:
         g = 1 #중력 시스템 ON
